26ec738740dd68a2b332b68249ec1ece81c12954
Micah Zaayer <micahzaayer@gmail.com>
2019-10-25 12:07:17 -0700

    add treats list to splash page.

--- SweetSavory/Controllers/HomeController.cs
+++ SweetSavory/Controllers/HomeController.cs
@@ -1,19 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Diagnostics;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc.Rendering;
+// using SweetSavory.Models;
+
+
 using Microsoft.AspNetCore.Mvc;
 using SweetSavory.Models;
-
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System;
 namespace SweetSavory.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SweetSavoryContext _db;
+
+        public HomeController(SweetSavoryContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet("/")]
         public ActionResult Index()
         {
-            return View();
+
+            return View(_db.Treats.ToList());
         }
     }
 }
