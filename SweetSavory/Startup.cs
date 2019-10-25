225ebfabb90f157d7d15e7f5c8a48c2bc9bf2d68
Micah Zaayer <micahzaayer@gmail.com>
2019-10-25 09:19:15 -0700

    add identity.

--- SweetSavory/Startup.cs
+++ SweetSavory/Startup.cs
@@ -31,6 +31,17 @@ namespace SweetSavory
             services.AddIdentity<ApplicationUser, IdentityRole>()
                       .AddEntityFrameworkStores<SweetSavoryContext>()
                       .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 0;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
         }
         public void Configure(IApplicationBuilder app)
         {
